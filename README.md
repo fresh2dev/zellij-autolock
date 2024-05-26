d9e7b1f54cc4adb27c6b105dadf969ca35f6a8d6
Donald Mellenbruch <hello@Fresh2.dev>
2024-05-26 00:00:00 +0000

    init

---
+++ README.md
@@ -0,0 +1,87 @@
+# zellij-autolock
+
+*zellij-autolock* is an experimental Zellij plugin that automatically switches between Zellij's "Normal" and "Locked" modes depending on the process running within the focused Zellij pane.
+
+I built *zellij-autolock* in pursuit of seamless navigation between Zellij panes and Vim windows. It works well for Vim, Neovim, and with other CLI applications that use keymaps that conflict with Zellij's keymaps including Helix, FZF, and more.
+
+> Note: When using with [Neo]vim, you'll also want to install this Vim plugin: [***zellij.vim***](https://github.com/fresh2dev/zellij.vim)
+
+## Demo
+
+Here is a demonstration of how you can seamlessly navigate through Zellij panes running Vim, Neovim, Helix, FZF, and more.
+
+<video autoplay="false" controls="controls" style="width: 800px;">
+  <source src="https://img.fresh2.dev/1716528665751_11894996682.webm" type="video/webm"/>
+  <p><i>This page does not support webm video playback.</i></p>
+  <p><i><a href="https://img.fresh2.dev/1716528665751_11894996682.webm" target="_blank">Click here to watch the demo recording.</a></i></p>
+</video>
+<p><b><i><a href="https://img.fresh2.dev/1716528665751_11894996682.webm" target="_blank">Open full screen demo recording.</a></i></b></p>
+
+Notice how the Zellij mode ( "Normal" or "Locked" in the top-right corner ) automatically toggles depending which process is running within the focused Zellij pane. This allows you to use the same mappings ( `Ctrl+h/j/k/l` ) to navigate between Zellij panes, Vim windows, FZF results, and more.
+
+## Install / Config
+
+> Note: Zellij >= 0.40.1 is required.
+
+This is a "headless" Zellij plugin; it has no UI. It is activated by pressing the `Enter` key in Zellij's "Normal" mode. Once activated, this plugin responds to Zellij events ( `TabUpdate` and `PaneUpdate` ) by examining the process running within the focused Zellij pane. If the running process is in either list `trigger` or `watch_trigger`, Zellij is then set to "Locked" mode. Otherwise it is unlocked (i.e., set to "Normal").
+
+I use the following Zellij config to intercept the "Enter" key, pass it through, then launch this plugin -- (an excerpt from [my dotfiles](https://github.com/fresh2dev/dotfiles)):
+
+```kdl
+plugins {
+    // Define the "autolock" plugin.
+    autolock location="https://github.com/fresh2dev/zellij-autolock/releases/download/latest/zellij-autolock.wasm" {
+        triggers "nvim|vim"  // Lock when any open these programs open. They are expected to unlock themselves when closed (e.g., using zellij.vim plugin).
+        watch_triggers "fzf|zoxide|atuin"  // Lock when any of these open and monitor until closed.
+        watch_interval "1.0"  // When monitoring, check every X seconds.
+    }
+    //...
+}
+keybinds {
+    normal {
+        bind "Enter" {  // Intercept `Enter`.
+            WriteChars "\u{000D}";  // Passthru `Enter`.
+            MessagePlugin "autolock" {};  // Activate the autolock plugin.
+        }
+        //...
+    }
+    //...
+}
+```
+
+The `triggers` setting allows a pipe-separated (`|`) list of CLI commands that will trigger Zellij's "Locked" mode.
+
+This plugin **can lock** Zellij when it encounters one of these commands, but it **cannot unlock** when the command exits. So, commands provided to `triggers` are expected to unlock Zellij on close. In the context of [Neo]vim, this is made possible with the Vim plugin [zellij.vim](https://github.com/fresh2dev/zellij.vim).
+
+The `watch_triggers` setting accounts for CLI commands that have no way of unlocking Zellij on close. When *zellij-autolock* encounters one of these commands, it puts Zellij into "Locked" mode and starts a timer that evaluates every `<watch_interval>` seconds. When the command exits, Zellij will unlock after the timer performs a cycle. This is intended for transient commands that benefit from `Ctrl+h/j/k/l` nav, like `fzf`, `zoxide`, and `atuin`.
+
+## Troubleshooting
+
+If you experience issues with the plugin, first try reloading it while bypassing the Zellij cache.
+
+```sh
+# From Github release:
+zellij plugin --skip-plugin-cache -- https://github.com/fresh2dev/zellij-autolock/releases/download/latest/zellij-autolock.wasm
+
+# From local file:
+zellij plugin --skip-plugin-cache -- file://$(pwd)/target/wasm32-wasi/release/zellij-autolock.wasm
+```
+
+[Zellij logs](https://zellij.dev/documentation/plugin-api-logging) are viewable here (on Linux):
+
+```sh
+tail -f /tmp/zellij-$(id -u)/zellij-log/zellij.log
+```
+
+## Future
+
+Today, this plugin uses Zellij CLI commands to determine the process running within the focused Zellij pane. When it is possible to use the Zellij API to make the distinction, I plan to refactor.
+
+This project is experimental and exists as a proof-of-concept. This is my first round of writing a Zellij plugin, or a project in Rust for that matter. If this project were to thrive, it would not be without community contributions. If this plugin solves [the Zellij / Vim navigation problem (#967)](https://github.com/zellij-org/zellij/issues/967), I dream that it'd just be baked into the Zellij project.
+
+## Shoutouts
+
+- [zellij-org/zellij](https://github.com/zellij-org/zellij)
+- [hiasr/vim-zellij-navigator](https://github.com/hiasr/vim-zellij-navigator)
+- [christoomey/vim-tmux-navigator](https://github.com/christoomey/vim-tmux-navigator)
+- [dj95/zjstatus](https://github.com/dj95/zjstatus)
---
+++ src/main.rs
@@ -0,0 +1,229 @@
+use std::collections::BTreeMap;
+use zellij_tile::prelude::*;
+
+/// Extracts the current running command from
+/// the CLI command `zellij action list-clients`.
+/// src: https://github.com/hiasr/vim-zellij-navigator/blob/main/src/main.rs
+fn term_command_from_client_list(cl: String) -> Option<String> {
+    let clients = cl.split('\n').skip(1).collect::<Vec<&str>>();
+    if clients.is_empty() {
+        return None;
+    }
+
+    let columns = clients[0].split_whitespace().collect::<Vec<&str>>();
+    if columns.len() < 3 {
+        return None;
+    }
+
+    let is_terminal = columns[1].starts_with("terminal");
+    let no_command = columns[2] == "N/A";
+    if !is_terminal || no_command {
+        return None;
+    }
+
+    let command = columns[2].split('/').last()?;
+    return Some(command.to_string());
+}
+
+fn list_clients() {
+    run_command(
+        &["zellij", "action", "list-clients"],
+        BTreeMap::from([("action".to_owned(), "list-clients".to_owned())]),
+    );
+}
+
+struct TabPane {
+    tab_pos: usize,
+    pane_id: u32,
+    term_cmd: Option<String>,
+}
+
+struct State {
+    permissions_granted: bool,
+    lock_trigger_cmds: Vec<String>,
+    lock_watch_cmds: Vec<String>,
+    watch_interval: f64,
+    timer_active: bool,
+    timer_scheduled: bool,
+    latest_tab_pane: TabPane,
+    latest_mode: InputMode,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            permissions_granted: false,
+            lock_trigger_cmds: vec!["vim".to_string(), "nvim".to_string()],
+            lock_watch_cmds: vec![],
+            watch_interval: 1.0,
+            timer_active: false,
+            timer_scheduled: false,
+            latest_tab_pane: TabPane {
+                tab_pos: usize::MAX,
+                pane_id: u32::MAX,
+                term_cmd: None,
+            },
+            latest_mode: InputMode::Normal,
+        }
+    }
+}
+
+register_plugin!(State);
+
+impl ZellijPlugin for State {
+    fn load(&mut self, configuration: BTreeMap<String, String>) {
+        request_permission(&[
+            PermissionType::RunCommands,
+            PermissionType::ChangeApplicationState,
+            PermissionType::ReadApplicationState,
+        ]);
+        subscribe(&[
+            EventType::PermissionRequestResult,
+            EventType::ModeUpdate,
+            EventType::TabUpdate,
+            EventType::PaneUpdate,
+            EventType::RunCommandResult,
+            EventType::Timer,
+        ]);
+        if self.permissions_granted {
+            hide_self();
+        }
+        self.load_configuration(configuration);
+    }
+
+    fn update(&mut self, event: Event) -> bool {
+        match event {
+            Event::PermissionRequestResult(permission) => {
+                self.permissions_granted = match permission {
+                    PermissionStatus::Granted => true,
+                    PermissionStatus::Denied => false,
+                };
+                if self.permissions_granted {
+                    hide_self();
+                }
+            }
+
+            Event::ModeUpdate(mode_info) => {
+                self.latest_mode = mode_info.mode;
+            }
+
+            Event::TabUpdate(tab_info) => {
+                if let Some(tab) = get_focused_tab(&tab_info) {
+                    if tab.position != self.latest_tab_pane.tab_pos {
+                        // eprintln!("Tab focus changed!");
+                        self.latest_tab_pane = TabPane {
+                            tab_pos: tab.position,
+                            pane_id: u32::MAX,
+                            term_cmd: None,
+                        };
+
+                        run_command(
+                            &["zellij", "action", "list-clients"],
+                            BTreeMap::from([("action".to_owned(), "list-clients".to_owned())]),
+                        );
+                    }
+                }
+            }
+
+            Event::PaneUpdate(pane_manifest) => {
+                let focused_pane =
+                    get_focused_pane(self.latest_tab_pane.tab_pos, &pane_manifest).clone();
+
+                if let Some(pane) = focused_pane {
+                    if pane.id != self.latest_tab_pane.pane_id
+                        || pane.terminal_command != self.latest_tab_pane.term_cmd
+                    {
+                        // eprintln!("Pane focus changed!");
+                        self.latest_tab_pane = TabPane {
+                            tab_pos: self.latest_tab_pane.tab_pos,
+                            pane_id: pane.id,
+                            term_cmd: pane.terminal_command,
+                        };
+
+                        list_clients();
+                    }
+                }
+            }
+
+            Event::RunCommandResult(_exit_code, stdout, _stderr, context) => {
+                if context.get("action") == Some(&"list-clients".to_owned()) {
+                    let stdout = String::from_utf8(stdout).unwrap();
+                    if stdout.len() == 0 {
+                        // The `list-clients` command sometimes bugs out and returns nothing.
+                        // When this happens, activate the timer to repeat another cycle.
+                        self.timer_active = true;
+                    } else {
+                        // eprintln!("list-clients output: {}", stdout);
+
+                        let pane_cmd = if let Some(cmd) = term_command_from_client_list(stdout) {
+                            cmd.clone()
+                        // } else if let Some(cmd) = &self.latest_tab_pane.term_cmd {
+                        //     cmd.split_whitespace().next().unwrap_or("").to_string()
+                        } else {
+                            "".to_string()
+                        };
+
+                        eprintln!("Detected command in pane: {}", pane_cmd);
+
+                        let is_watched_cmd = self.lock_watch_cmds.contains(&pane_cmd);
+
+                        let target_input_mode =
+                            if is_watched_cmd || self.lock_trigger_cmds.contains(&pane_cmd) {
+                                InputMode::Locked
+                            } else if self.latest_mode == InputMode::Locked {
+                                InputMode::Normal
+                            } else {
+                                self.latest_mode
+                            };
+
+                        if self.latest_mode != target_input_mode {
+                            switch_to_input_mode(&target_input_mode);
+                        }
+
+                        self.timer_active = is_watched_cmd;
+                    }
+
+                    if self.timer_active && !self.timer_scheduled {
+                        set_timeout(self.watch_interval);
+                        self.timer_scheduled = true;
+                    }
+                }
+            }
+
+            Event::Timer(_t) => {
+                list_clients();
+                self.timer_scheduled = false;
+            }
+
+            _ => {}
+        }
+        return false; // No need to render UI.
+    }
+
+    fn pipe(&mut self, _pipe_message: PipeMessage) -> bool {
+        list_clients();
+        return false; // No need to render UI.
+    }
+
+    fn render(&mut self, _rows: usize, _cols: usize) {}
+}
+
+impl State {
+    fn load_configuration(&mut self, configuration: BTreeMap<String, String>) {
+        if let Some(lock_trigger_cmds) = configuration.get("triggers") {
+            self.lock_trigger_cmds = lock_trigger_cmds
+                .split('|')
+                .map(|s| s.trim().to_string())
+                .collect();
+        }
+        if let Some(lock_watch_cmds) = configuration.get("watch_triggers") {
+            self.lock_watch_cmds = lock_watch_cmds
+                .split('|')
+                .map(|s| s.trim().to_string())
+                .collect();
+        }
+        if let Some(watch_interval) = configuration.get("watch_interval") {
+            self.watch_interval = watch_interval.parse::<f64>().unwrap();
+        }
+    }
+}
